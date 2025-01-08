c1e233b84ae980d5e9a308f585755b67e2d11f6f
Ted Szylowiec <tedszy@gmail.com>
2025-01-07 21:30:01 -0500

    New clos interface, zgroup, agroup, mgroup...

--- multiplicative-groups.lisp
+++ multiplicative-groups.lisp
@@ -1,7 +1,7 @@
 (in-package :mgroups)
 
 ;; This package is for doing simple computations on small
-;; multiplicative residue groups mod n.
+;; integer residue groups mod n (additive or multiplicative.)
 ;;
 ;; A multiplicative resudie group mod n is denoted by Z/nZ*.
 ;; Take all the reduced residues mod n and strike out the
@@ -17,8 +17,10 @@
 ;;
 ;; For example we can use Z/nZ* groups to illustrage Lagrange's theorem.
 ;;
-;; For convenience/comparison we add ability to create
-;; and work with (Z/nZ, modn+) groups as well. 
+;; Additive Z/nZ...
+;;
+;; (to do) ...  docs, comments.
+;;
 
 (declaim (optimize (debug 3)))
 ;; (declaim (optimize (speed 3)))
@@ -57,7 +59,7 @@ multiplicative residues group mod n."
   "Create binary operation (x + y) mod n."
   (lambda (x y) (mod (+ x y) n)))
 
-(defun cayley-table (set op)
+(defun make-cayley-table (set op)
   "Computes Cayley table for the set with binary operation op*."
   (let ((table (loop for x in set
 		     collecting (loop for y in set
@@ -72,215 +74,140 @@ multiplicative residues group mod n."
 
 (defun z/nz*-cayley-table (n)
   "Convenience function: get the Cayley table of Z/nZ* immediately."
-  (cayley-table (z/nz*-residues n) (mod-binary-op-* n)))
+  (make-cayley-table (z/nz*-residues n) (mod-binary-op-* n)))
 
 (defun z/nz-cayley-table (n)
   "Convenience function: get the Cayley table of Z/nZ immediately."
-  (cayley-table (z/nz-residues n) (mod-binary-op-+ n)))
+  (make-cayley-table (z/nz-residues n) (mod-binary-op-+ n)))
 
-;; We build a nice interface for interactive use with CLOS.
-;; An MGROOP or AGROUP instance is an object with a set of
-;; elements and a binary operation.
-;;
-;; These objects are initialized by passing a modulus
-;; to initialize-instance.
+;; We build an interface for interactive use with CLOS.
 
-(defclass mgroup ()
-  ((modulus :initarg :modulus :accessor modulus)
+(defclass zgroup ()
+  ((binary-op :initarg :binary-op :accessor binary-op)
+   (binary-op-label :initarg :binary-op-label :accessor binary-op-label)
    (elements :initarg :elements :accessor elements)
-   (binary-op :accessor binary-op)
-   (order :initarg :order :accessor order))
-  (:documentation "Multiplicative group mod m."))
+   (id :initarg :id :accessor id)
+   (order :initarg :order :accessor order)
+   (cayley-table :initarg :cayley-table :accessor cayley-table))
+  (:documentation "Class that can hold an integer group."))
 
+;; (defmethod initialize-instance :after ((zg zgroup) &key)
+;;   "Create the Cayley-table and find the order."
+;;   (with-slots (binary-op elements cayley-table order)
+;;       zg
+;;     (setf cayley-table (make-cayley-table elements binary-op))
+;;     (setf order (length elements))))
 
-(defclass agroup ()
-  ((modulus :initarg :modulus :accessor modulus)
-   (elements :initarg :elements :accessor elements)
-   (binary-op :accessor binary-op)
-   (order :initarg :order :accessor order))
-  (:documentation "Additive group mod m."))
+(defclass agroup (zgroup)
+  ((modulus :initarg :modulus :accessor modulus)))
 
-;; The binary operation is automatically computed
-;; from the modulus.
+(defclass mgroup (zgroup)
+  ((modulus :initarg :modulus :accessor modulus)))
 
-(defmethod initialize-instance :after ((obj mgroup) &key)
-  "Create a binary multiplication operation with object's modulus."
-  (with-slots (modulus binary-op)
-      obj
-    (setf binary-op (mod-binary-op-* modulus))))
-
-(defmethod initialize-instance :after ((obj agroup) &key)
-  "Create a binary addition operation with object's modulus."
-  (with-slots (modulus binary-op)
-      obj
-    (setf binary-op (mod-binary-op-+ modulus))))
-
-(defmethod print-object ((obj mgroup) stream)
-  (print-unreadable-object (obj stream :type nil)
-    (with-slots (modulus elements order)
-	obj
-      (format stream "|~a| M*~a ~a" order modulus elements))))
-
-(defmethod print-object ((obj agroup) stream)
-  (print-unreadable-object (obj stream :type nil)
-    (with-slots (modulus elements order)
-	obj
-      (format stream "|~a| A+~a ~a" order modulus elements))))
-
-;; Constructor functions.
-
-(defun make-z/nz*-group (n)
-  "Constructs a Z/nZ* group with * mod n binary operation."
-  (let ((elements (z/nz*-residues n)))
-    (make-instance 'mgroup :modulus n
-			   :elements elements
-			   :order (length elements))))
-
-(defun make-z/nz-group (n)
-  "Constructs a Z/nZ group with + mod n binary operation."
-  (let ((elements (z/nz-residues n)))
-    (make-instance 'agroup :modulus n
-			   :elements elements
-			   :order (length elements))))
-
-;; Generic interfaces.
-
-(defgeneric make-subgroup (group elements)
-  (:documentation "returns mgroup where elements are subset of mgroup's elements
-with the same binary operation."))
-
-(defgeneric compute-cayley-table (group)
-  (:documentation "Compute the Cayley table of the group."))
-
-(defgeneric print-cayley-table (group &key)
-  (:documentation "Prints out Cayley table with specified field width."))
-
-(defgeneric find-subgroups (group order)
-  (:documentation "Find subgroups of a group having give order."))
-
-;; Methods.
-
-(defmethod compute-cayley-table ((mg mgroup))
-  "Returns a Cayley table as a list of lists."
-  (cayley-table (elements mg) (binary-op mg)))
-
-(defmethod compute-cayley-table ((ag agroup))
-  "Cayley table for a + mod n AGROUP."
-  (cayley-table (elements ag) (binary-op ag)))
-
-(defmethod print-cayley-table ((mg mgroup) &key (width 5))
-  "Display Cayley table to standard output."
-  (display-table (cayley-table (elements mg) (binary-op mg)) :width width))
-
-(defmethod print-cayley-table ((ag agroup) &key (width 5))
-  "Display Cayley table to standard output."
-  (display-table (cayley-table (elements ag) (binary-op ag)) :width width))
-
-(defmethod make-subgroup ((mg mgroup) elements)
-  "Mgroups are not always the full Z/nZ*. A smaller subset
-of the elements may also be a group under the same binary operation.
-And so we need a way to create mgroups from arbitrary element sets."
-  (make-instance 'mgroup :modulus (modulus mg)
+(defun make-zgroup (elements binary-op id &key (binary-op-label "@"))
+  (make-instance 'zgroup :binary-op binary-op
 			 :elements elements
+			 :id id
+			 :cayley-table (make-cayley-table elements binary-op)
+			 :binary-op-label binary-op-label
 			 :order (length elements)))
 
-(defmethod make-subgroup ((ag agroup) elements)
-  "AGROUPs are not allways the full Z/nZ. For example the set (0) 
-under any mod n + is a group."
-  (make-instance 'agroup :modulus (modulus ag)
-			 :elements elements
-			 :order (length elements)))
+(defun make-agroup (n)
+  (let ((elements (z/nz-residues n))
+	(op (mod-binary-op-+ n)))
+    (make-instance 'agroup :binary-op op
+			    :elements elements
+			    :id 0
+			    :modulus n
+			    :order (length elements)
+			    :cayley-table (make-cayley-table elements op)
+			    :binary-op-label (format nil "MOD+~a" n))))
 
-;; Test if a set of elements and a binary operation is a group.
+(defun make-mgroup (n)
+  (let ((elements (z/nz*-residues n))
+	(op (mod-binary-op-* n)))
+    (make-instance 'mgroup :binary-op op
+			    :elements elements
+			    :id 1
+			    :modulus n
+			    :order (length elements)
+			    :cayley-table (make-cayley-table elements op)
+			    :binary-op-label (format nil "MOD*~a" n))))
 
-(defun has-1-p (set)
-  "Mgroups have to have 1, the identity."
-  (member 1 set))
+(defmethod print-object ((zg zgroup) stream)
+  (print-unreadable-object (zg stream :type nil)
+    (with-slots (order binary-op-label id elements)
+	zg
+      (format stream "ZG ~a ~a ~d ~a" order binary-op-label id elements))))
 
-(defun has-0-p (set)
-  "AGROUPs have to have 0, the identity."
-  (member 0 set))
+(defmethod print-object ((ag agroup) stream)
+  (print-unreadable-object (ag stream :type nil)
+    (with-slots (order binary-op-label id elements)
+	ag
+      (format stream "AG ~a ~a ~d ~a" order binary-op-label id elements))))
 
-(defun has-closure-p (set op)
-  "All the elements in the Cayley table must be elements of the given set."
-  (loop for e in set
-	always (loop for f in set
-		     always (member (funcall op e f) set))))
+(defmethod print-object ((mg mgroup) stream)
+  (print-unreadable-object (mg stream :type nil)
+    (with-slots (order binary-op-label id elements)
+	mg
+      (format stream "MG ~a ~a ~d ~a" order binary-op-label id elements))))
 
-;; Since mgroups are Abelian, we only need to check for right inverses.
+(defun print-cayley-table (group &key (width 5))
+  (with-slots (binary-op cayley-table)
+      group
+    (display-table cayley-table :width width)))
 
-(defun has-right-1-inverses-p (set op)
-  "Every element in the set must have exactly one right inverse."
-  (loop for e in set
-	always (= 1 (loop for f in set counting (= 1 (funcall op e f))))))
+(defun make-subgroup (group elements)
+  (with-slots (binary-op id binary-op-label)
+      group
+    (make-zgroup elements binary-op id :binary-op-label binary-op-label)))
 
-(defun has-right-0-inverses-p (set op)
-  "Every element in the set must have exactly one right inverse."
-  (loop for e in set
-	always (= 1 (loop for f in set counting (= 0 (funcall op e f))))))
+(defun has-identity? (id elements)
+  (member id elements))
 
+(defun is-closed? (op elements)
+  (loop for e in elements
+	always (loop for f in elements
+		     always (member (funcall op e f) elements))))
 
-(defun is-mgroup-p (set op)
-  "Does <set, op> form a group?"
-  (and (has-1-p set)
-       (has-closure-p set op)
-       (has-right-1-inverses-p set op)))
+(defun has-right-inverses? (op id elements)
+  (loop for e in elements
+	always (= 1 (loop for f in elements counting (= id (funcall op e f))))))
 
-(defun is-agroup-p (set op)
-  "Does <set, op> form a group?"
-  (and (has-0-p set)
-       (has-closure-p set op)
-       (has-right-0-inverses-p set op)))
+(defun is-subgroup? (op id elements)
+  (and (has-identity? id elements)
+       (is-closed? op elements)
+       (has-right-inverses? op id elements)))
 
-;; This can be made faster by using the following idea:
-;; we know 1 must be in any subgroup, so we remove it and
-;; find combinations of length k from the n-1 other elements.
+(defun find-subgroups (group k)
+  "Find all subgroups of order k by brute force."
+  (with-slots (binary-op id elements)
+      group
+    (let ((result nil))
+      (alexandria:map-combinations
+       #'(lambda (element-list)
+	   (when (is-subgroup? binary-op id element-list)
+	     (push (make-subgroup group element-list) result)))
+       elements
+       :length k)
+      result)))
 
-(defmethod  find-subgroups ((mg mgroup) (k integer))
-  "Find all subgroups of order k by checking all combinations of lenth k
-from the set of elements. Return a list of them."
-  (let ((result nil))
-    (alexandria:map-combinations
-     #'(lambda (set)
-	 (when (is-mgroup-p set (binary-op mg))
-	   (push (make-subgroup mg set) result)))
-     (elements mg)
-     :length k)
-    (reverse result)))
-
-(defmethod  find-subgroups ((ag agroup) (k integer))
-  "Find all subgroups of order k by checking all combinations of lenth k
-from the set of elements. Return a list of them."
-  (let ((result nil))
-    (alexandria:map-combinations
-     #'(lambda (set)
-	 (when (is-Agroup-p set (binary-op ag))
-	   (push (make-subgroup ag set) result)))
-     (elements ag)
-     :length k)
-    (reverse result)))
-
-
-
-;; Convenience functions.
-
-(defun z/nz*-show (n &key (width 5))
-  (let ((foo (make-z/nz*-group n)))
+(defun mg-show (n &key (width 5))
+  (let ((foo (make-mgroup n)))
     (print-cayley-table foo :width width)
     foo))
 
-(defun z/nz-show (n &key (width 5))
-  (let ((foo (make-z/nz-group n)))
+(defun ag-show (n &key (width 5))
+  (let ((foo (make-agroup n)))
     (print-cayley-table foo :width width)
     foo))
-
 
 ;; Tests ==================================================================
 
-(5am:def-suite mgroups-test-suite
+(5am:def-suite zgroups-test-suite
   :description "Check how well multiplicative group theory functions work.")
 
-(5am:in-suite mgroups-test-suite)
+(5am:in-suite zgroups-test-suite)
 
 (5am:def-test check-set-p ()
   (5am:is (set-p '(1 2 3 4)))
@@ -307,20 +234,7 @@ from the set of elements. Return a list of them."
     (5am:is (= 6 (funcall mod7+ x y)))
     (5am:is (= 0 (funcall mod15+ x y)))))
 
-(5am:def-test is-it-a-group? ()
-  (let ((op* (mod-binary-op-* 9))
-	(op+ (mod-binary-op-+ 9)))
-    (5am:is (has-1-p '(1 2 3 4)))
-    (5am:is (not (has-1-p '(2 3 4))))
-    (5am:is (has-0-p '(0 1 2 3 4)))
-    (5am:is (not (has-0-p '(1 2 3 4))))
-    (5am:is (has-closure-p '(1 2 4 5 7 8) op*))
-    (5am:is (not (has-closure-p '(1 2 4 5 7) op*)))
-    (5am:is (has-right-1-inverses-p '(1 2 4 5 7 8) op*))
-    (5am:is (not (has-right-1-inverses-p '(1 2 4 5 6 7 8) op*)))
-    (5am:is (has-right-0-inverses-p '(0 1 2 3 4 5 6 7 8) op+))
-    (5am:is (not (has-right-0-inverses-p '(0 1 3 4 5 6 7 8) op+)))))
+;; Just run this to run the tests.
 
-
-(defun do-mgroups-tests ()
-  (5am:run! 'mgroups-test-suite))
+(defun do-zgroups-tests ()
+  (5am:run! 'zgroups-test-suite))
